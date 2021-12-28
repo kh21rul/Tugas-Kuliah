27eb1e87e1856372c9752df2428664b8e0471ed1
Khairul Aqram <khairulaqram21@gmail.com>
2021-12-28 10:34:49 +0700

    Belajar Database Mysql dari Netbeans

---
+++ Pemograman_Berorientasi_Object/Jadwal_Kuliah/src/Latihan_Database/KoneksiDB.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan_Database;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Aqram
+ */
+public class KoneksiDB {
+    private Connection koneksi;
+ 
+ public Connection getKoneksi(){
+ return koneksi;
+ }
+ 
+ public void koneksiDatabase() { //<-- untuk koneksi ke database
+ // Cek Driver
+ try {
+ Class.forName("com.mysql.jdbc.Driver"); //<-- nama driver untuk koneksi ke MySQL
+ // Cek Database
+ try {
+ String url, user, password;
+ url = 
+"jdbc:mysql://localhost:3307/Jadwal_Kuliah"; //alamat DB
+ user = "root";
+ password = "";
+ koneksi = DriverManager.getConnection(url, user, password);
+ System.out.println("Koneksi Sukses");
+ } catch (SQLException se) {
+ JOptionPane.showMessageDialog(null, 
+"Koneksi Gagal! " + se);
+ System.exit(0);
+ }
+ } catch (ClassNotFoundException cnfe) {
+ JOptionPane.showMessageDialog(null, "Driver Tidak Ditemukan!" +cnfe);
+ System.exit(0);
+ }
+ }
+ public static void main(String[] kon) {
+ new KoneksiDB().koneksiDatabase();
+ }
+    
+    
+}
